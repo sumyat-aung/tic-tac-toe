d133b27004464a17105ed0c2df98e06e832e3f5f
sumyataung2004 <sumyataung1584@gmail.com>
2022-08-30 00:14:37 +0630

    2nd commit

---
+++ README.md
@@ -0,0 +1,34 @@
+# TIC | TAC | TOE
+
+## Overview
+
+  <h3>   Demo - URL link -
+    <a href="https://tictactoe-by-sumyat.netlify.app/">
+     https://tictactoe-by-sumyat.netlify.app/
+    </a>
+  </h3>
+
+<br/>
+<br/>
+
+![](Demo/large-screen.png)
+<br/>
+<br/>
+
+<div align="center">
+<img src="Demo/small-screen.png" width="300">
+</div>
+
+<br/>
+<br/>
+
+## Built With
+
+- **HTML**
+- **CSS**
+- **JS**
+
+## Features
+
+- **Responsive and look great on all devices**
+- **2 player Tic Tac Toe game**
--- script.js
+++ script.js
@@ -2,7 +2,88 @@ const playAgain = document.getElementById("playAgain");
 const secoundPage = document.getElementsByClassName("secondPage")[0];
 const firstPage = document.getElementById("board");
 
+// start game
+
+let cell = document.getElementsByClassName("cell");
+let cellAsArray = Array.from(cell);
+
+let board = ["", "", "", "", "", "", "", "", ""];
+
+let CurrentPlayer = "X";
+
+const winOrLose = document.getElementById("winOrLose");
+
+cellAsArray.forEach((eachCell, index) => {
+  eachCell.addEventListener("click", () => {
+    let id = eachCell.id;
+    if (!board[id]) {
+      board[id] = CurrentPlayer;
+      eachCell.innerHTML = `<p>${CurrentPlayer}</p>`;
+
+      if (playerhasWon()) {
+        secoundPage.style.display = "flex";
+        winOrLose.textContent = `${CurrentPlayer} HAS WON !  `;
+        return;
+      }
+
+      CurrentPlayer = CurrentPlayer === "X" ? "O" : "X";
+    }
+  });
+});
+
+// indexes winthin the board
+//   [0] [1] [2]
+//   [3] [4] [5]
+//   [6] [7] [8]
+
+// Winning algorithm
+
+let playerhasWon = () => {
+  if (board[0] === CurrentPlayer) {
+    if (board[1] === CurrentPlayer && board[2] === CurrentPlayer) {
+      return true;
+    }
+    if (board[3] === CurrentPlayer && board[6] === CurrentPlayer) {
+      return true;
+    }
+    if (board[4] === CurrentPlayer && board[8] === CurrentPlayer) {
+      return true;
+    }
+  }
+
+  if (board[8] === CurrentPlayer) {
+    if (board[5] === CurrentPlayer && board[2] === CurrentPlayer) {
+      return true;
+    }
+    if (board[7] === CurrentPlayer && board[6] === CurrentPlayer) {
+      return true;
+    }
+  }
+
+  if (board[4] === CurrentPlayer) {
+    if (board[3] === CurrentPlayer && board[5] === CurrentPlayer) {
+      return true;
+    }
+    if (board[1] === CurrentPlayer && board[7] === CurrentPlayer) {
+      return true;
+    }
+    if (board[2] === CurrentPlayer && board[6] === CurrentPlayer) {
+      return true;
+    }
+  }
+};
+
+// reset btn
+
 playAgain.addEventListener("click", () => {
+  board.forEach((eachBoard, index) => {
+    board[index] = "";
+  });
+
+  cellAsArray.forEach((eachCell, index) => {
+    eachCell.innerHTML = "";
+  });
+
   secoundPage.style.display = "none";
   firstPage.style.display = "grid";
 });
